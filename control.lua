edb10ffc7dc3b547134e3bf44893271823c1c0f5
Jan Mischak <jan.mischak@kstp.at>
2020-05-26 17:47:04 +0200

    Fix items potentially getting deleted
    Remove script.raise_event

--- control.lua
+++ control.lua
@@ -1,6 +1,4 @@
 
-require("__RaiseEventProtection__/protection.lua")
-
 local function try_get_copy_source(player)
   local copy_source = player.entity_copy_source
   if copy_source then
@@ -16,15 +14,34 @@ script.on_event(defines.events.on_player_selected_area, function (event)
     local copy_source = try_get_copy_source(player)
     if copy_source then
       local copy_source_name = copy_source.name
-      local raise_data = {
-        player_index = player_index,
-        source = copy_source,
-      }
+      local player_position = player.position
+      local force = player.force
+      local player_insert = player.insert
+      local spill_item_stack = player.surface.spill_item_stack
+      local simple_item_stack = {}
       for _, entity in pairs(event.entities) do
         if entity.name == copy_source_name then
-          entity.copy_settings(copy_source)
-          raise_data.destination = entity
-          script.raise_event(defines.events.on_entity_settings_pasted, raise_data)
+          local returned_items = entity.copy_settings(copy_source, player)
+          if next(returned_items) then
+            if player.can_reach_entity(entity) then
+              for name, count in pairs(returned_items) do
+                simple_item_stack.name = name
+                simple_item_stack.count = count
+                count = count - player_insert(simple_item_stack)
+                if count > 0 then
+                  simple_item_stack.count = count
+                  spill_item_stack(player_position, simple_item_stack, false, force, false)
+                end
+              end
+            else
+              local entity_position = entity.position
+              for name, count in pairs(returned_items) do
+                simple_item_stack.name = name
+                simple_item_stack.count = count
+                spill_item_stack(entity_position, simple_item_stack, false, force, false)
+              end
+            end
+          end
         end
       end
     end
