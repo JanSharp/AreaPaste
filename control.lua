f6cfe621806cd5fcbaa0931bb5ca5f83b649c7e4
JanSharp <steinmeister36@gmail.com>
2022-09-12 23:09:45 +0200

    Add entity and surface valid checks everywhere
    Update license copy right year

--- control.lua
+++ control.lua
@@ -11,19 +11,23 @@ script.on_event(defines.events.on_player_selected_area, function (event)
   if event.item == "AreaPaste-paste-planner" then
     local player_index = event.player_index
     local player = game.get_player(player_index)
+    ---@cast player -nil
     local copy_source = try_get_copy_source(player)
     if copy_source then
       local copy_source_name = copy_source.name
       local player_position = player.position
       local force = player.force
       local player_insert = player.insert
-      local spill_item_stack = player.surface.spill_item_stack
+      local surface = player.surface
+      local spill_item_stack = surface.spill_item_stack
       local simple_item_stack = {}
       for _, entity in pairs(event.entities) do
-        if entity.name == copy_source_name then
+        if entity.valid and entity.name == copy_source_name then
+          local entity_position = entity.position
+          -- `copy_settings` raises an event, so every LuaObject could be invalidated by this call
           local returned_items = entity.copy_settings(copy_source, player)
-          if next(returned_items) then
-            if player.can_reach_entity(entity) then
+          if next(returned_items) and surface.valid then
+            if entity.valid and player.can_reach_entity(entity) then
               for name, count in pairs(returned_items) do
                 simple_item_stack.name = name
                 simple_item_stack.count = count
@@ -34,7 +38,6 @@ script.on_event(defines.events.on_player_selected_area, function (event)
                 end
               end
             else
-              local entity_position = entity.position
               for name, count in pairs(returned_items) do
                 simple_item_stack.name = name
                 simple_item_stack.count = count
@@ -42,6 +45,9 @@ script.on_event(defines.events.on_player_selected_area, function (event)
               end
             end
           end
+        end
+        if not copy_source.valid then
+          break
         end
       end
     end
